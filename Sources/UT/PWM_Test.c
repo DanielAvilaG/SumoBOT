f2db7d0b316f73318ad17362083c176894a20c25
Nilo33 <nose333@hotmail.es>
2019-05-20 14:08:20 +0200

    Add files via upload

--- Sources/UT/PWM_Test.c
+++ Sources/UT/PWM_Test.c
@@ -8,14 +8,44 @@
 
 int PWM_test(void)
 {
-	UART0_send_string_ln("\t\033[33mMotors\033[0m");
-	PWM_duty(10000,0);
-	delayMs(1000);
-	PWM_duty(10000,10000);
-	delayMs(1000);
-	PWM_duty(10000,-10000);
-	delayMs(1000);
-	PWM_duty(0,0);
-	
+	char c;
+		for(;;) {     
+
+			UART0_send_string_ln("Select Mode:");
+			UART0_send_string_ln("'w': Endavant \t 'a': esquerra \t 's': Enrrere\t 'd' dreta\t 'x': EXIT TESTS");
+			
+			
+			while(!(UART0_S1 & UART0_S1_RDRF_MASK)) { } /* 0x20 wait for receive buffer full */
+			c = UART0_D; /* read the char received */
+			
+			switch (c){
+			case 'w':
+				PWM_duty(-20000,-20000);//endavant
+				break;
+				
+			case 'a':
+				PWM_duty(20000,-20000);//esquerra
+				break;
+				
+			case 's':
+				PWM_duty(20000,20000);//endarrera
+				break;
+			case 'a':
+				PWM_duty(-20000,20000);//dreta
+				break;
+							
+			case 'x':
+				return;
+				break;
+			
+			default:
+				UART0_send_ln();
+				UART0_send_string("\t\033[37;1;5m");
+				UART0_send_char(c);
+				UART0_send_string_ln("\033[0m\033[31m IS NOT AN OPTION!\033[0m");
+				UART0_send_ln();
+				break;
+			}
+		}
 	return 0;
 }
